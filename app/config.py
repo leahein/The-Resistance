af213c8e2eff3ff0a73f9b0785e0fe126223508d
Leah <leaheinhorn@yahoo.com>
2017-11-27 13:38:19 -0500

    initial commit for view of players from google sheet

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
 
 import yaml
+from services.google_sheet import google_sheet_client
 
 ######################################################################
 # Paths
@@ -9,9 +10,21 @@ PATH_ROOT = os.path.dirname(os.path.abspath(__file__))
 PATH_INSTANCE = os.path.join(PATH_ROOT, 'instance')
 
 PATH_SECURE = os.path.join(PATH_INSTANCE, 'secure.yaml')
+PATH_GOOGLE_SHEETS_SECURE = os.path.join(PATH_INSTANCE, 'google-sheets.json')
 
+
+######################################################################
+# Secure Credentials
+######################################################################
 
 with open(PATH_SECURE, 'r') as PATH_SECURE_FILE_OBJ:
     SECURE_CONFIG = yaml.load(PATH_SECURE_FILE_OBJ.read())
 
 AWS = SECURE_CONFIG['aws']
+GS_BOOK_CODE = GS_BOOK_CODE['google_sheet']['book_code']
+
+######################################################################
+# Clients
+######################################################################
+
+GS_CLIENT = google_sheet_client(PATH_GOOGLE_SHEETS_SECURE)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
+from flask import render_template
+
 from app import app
+from services.google_sheet import GoogleSpreadsheet
+import config
 
 @app.route('/')
 @app.route('/game/new')
 def new():
-    return "Hello, World!"
+    team_data = GoogleSpreadsheet(
+        book_code=config.GS_BOOK_CODE,
+        client=config.GS_CLIENT
+    ).get_worksheet_data('kepler')
+    render_template(
+        'game/new.html',
+        players=[player['Name'] for player in team_data]
+    )
